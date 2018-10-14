94adefe8d9e55b76b8e2f7c3f0eeaa8189e4e93d
Shyrik <r.wesenberg@gmail.com>
2018-10-14 19:16:20 +0200

    ingots now render inside the crucible

--- src/main/java/com/camellias/mysticalmetallurgy/common/block/crucible/RendererCrucible.java
+++ src/main/java/com/camellias/mysticalmetallurgy/common/block/crucible/RendererCrucible.java
@@ -1,7 +1,27 @@
 package com.camellias.mysticalmetallurgy.common.block.crucible;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.block.model.ItemCameraTransforms;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 
 public class RendererCrucible extends TileEntitySpecialRenderer<TileCrucible>
 {
+    @Override
+    public void render(TileCrucible te, double x, double y, double z, float partialTicks, int destroyStage, float alpha)
+    {
+        for(int slot = 0; slot < TileCrucible.INPUT_SLOTS; slot++)
+        {
+            GlStateManager.pushMatrix();
+            //Translate into the centre of the inner crucible, above the bottom
+            GlStateManager.translate(x + 0.45F + 0.075F * slot, y + 0.6F, z + 0.5F);
+            //make them diagonal
+            GlStateManager.rotate(90.0F, 0.0F, 0.0F, 1.0F);
+            GlStateManager.rotate(50.0F, 1.0F, 1.0F, 0.0F);
+            //make them fit
+            GlStateManager.scale(0.3F, 0.3F, 0.3F);
+            Minecraft.getMinecraft().getRenderItem().renderItem(te.input.getStackInSlot(slot), ItemCameraTransforms.TransformType.FIXED);
+            GlStateManager.popMatrix();
+        }
+    }
 }
