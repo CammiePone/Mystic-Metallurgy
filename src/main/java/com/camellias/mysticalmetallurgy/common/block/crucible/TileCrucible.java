4b8751316fcc8dbb8de69f5b84865df5e93b6e24
Shyrik <r.wesenberg@gmail.com>
2018-10-16 13:58:25 +0200

    forge style blockstate files

--- src/main/java/com/camellias/mysticalmetallurgy/common/block/crucible/TileCrucible.java
+++ src/main/java/com/camellias/mysticalmetallurgy/common/block/crucible/TileCrucible.java
@@ -13,7 +13,6 @@ import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ITickable;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.fluids.FluidStack;
